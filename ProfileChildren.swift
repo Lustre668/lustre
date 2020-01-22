5adbbe6f125e5896ce656edff5b9e819e6809c1c
Ghaida <ghaida.unv@gmail.com>
2020-01-22 10:28:32 +0300

    Ghaida Test push / four screens

---
+++ ProfileChildren.swift
@@ -0,0 +1,9 @@
+//
+//  ProfileChildren.swift
+//  lustre
+//
+//  Created by ghaida habes on 21/01/2020.
+//  Copyright © 2020 Shahad X. All rights reserved.
+//
+
+import Foundation
---
+++ lustre/profileChildren.swift
@@ -0,0 +1,32 @@
+//
+//  ProfileChildren.swift
+//  lustre
+//
+//  Created by ghaida habes on 21/01/2020.
+//  Copyright © 2020 Shahad X. All rights reserved.
+//
+
+import UIKit
+
+class ProfileChildren: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.title = " Lustre "
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
